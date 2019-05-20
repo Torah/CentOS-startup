d24edf51166195ca2ee494856ec474b15e47c43c
root <admin@example.com>
2019-05-20 10:03:58 +0800

    v 0.2

--- CentOS.sh
+++ CentOS.sh
@@ -6,18 +6,43 @@ sudo yum upgrade
 sudo yum remove docker docker-common docker-selinux docker-engine
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 
 yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh && bash install.sh
 
-wget http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el7/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-sudo yum install rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-sudo yum install fuse-ntfs-3g
 
+# 下面的安装包命令行可以让CentOS识别NTF格式分区
+wget http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el7/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+sudo yum install -y rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+sudo yum install -y fuse-ntfs-3g
+
+
+# 安装多媒体播放器
 yum update
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
-sudo yum install vlc
+sudo yum -y install vlc
+
+
+# 安装向日葵被控端
+wget http://download.oray.com/sunlogin/linux/SunloginClient9.6.1.tar.gz
+tar -zxvf SunloginClient9.6.1.tar.gz
+cd sunloginclient
+./install.sh
+
+# 安装teamviewer
+wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
+sudo yum install -y teamviewer.x86_64.rpm
+
+docker pull jellyfin/jellyfin
+docker pull oldiy/my-calibre-webserver-docker
+docker pull zexi/listen1:v1
+docker pull docker.io/tianon/grafana:latest
+docker pull gitlab/gitlab-ce:10.8.0-ce.0
+docker pull kishitat/wordpress-all-in-one
+mvertes/alpine-mongo:latest
+docker pull sebp/elk:640
+docker pull sebp/elk:670
 
